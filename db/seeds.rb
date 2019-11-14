32201761f73a5ef1c6c16b19939a249f1de56506
mehdi sijelmassi <mehdisijelmassi1@gmail.com>
2019-11-14 11:03:35 +0100

    Exercice done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '44 20 7420 9324',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+44 20 7729 1888',
+    category:     'chinese'
+  },
+  {
+    name:         'Boca Mexa',
+    address:      '7 rue Oberkampf, Paris 75011',
+    phone_number: '01 77 14 73 24',
+    category:     'french'
+  },
+  {
+    name:         'Le Per Se',
+    address:      '10 Columbus Cir, New York, NY 10019',
+    phone_number: '1 212-823-9335',
+    category:     'french'
+  },
+  {
+    name:         'Comptoir Italien',
+    address:      '3 Rue Jean-Jacques Rousseau, 75001 Paris',
+    phone_number: '05 34 66 79 58',
+    category:     'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
